df542e0bc1bbd6c7765c329e56d16fe2d0c54000
KamilGmitrzuk <123082737+KamillGG@users.noreply.github.com>
2023-10-05 15:42:36 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+async function funk(){
+    const data = await fetch('http://localhost/kgwordpress/wp-json/wp/v2/posts',)
+    const json = await data.json()
+    console.log(json)
+    for(let i=0;i<=json.length-1;i++){
+        const point = document.createElement('div')
+        const li = document.createElement('li')
+        const butt = document.createElement('button')
+        li.innerHTML = json[i].title.rendered
+        butt.innerHTML = "DELETE"
+        butt.addEventListener('click',()=>{
+            deleteArticle(json[i].id)
+        })
+        point.setAttribute('id',json[i].id)
+        point.appendChild(li)
+        point.appendChild(butt)
+        document.getElementById('list').appendChild(point)
+    }
+}
+async function deleteArticle(id){
+    const data = await fetch(`http://localhost/kgwordpress/wp-json/wp/v2/posts/${id}`,{
+        method:"delete",
+        headers:{
+            Authorization:`Basic ${btoa("Kamil:koronawirus2@2@")}`
+        }
+    })
+    const json =await data.json()
+    if(json.status=="trash"){
+        document.getElementById(json.id).remove()
+    }
+}
+async function add(){
+    const tytul =document.getElementById('tytul').value
+    const tresc = document.getElementById('tresc').value
+    const data =await fetch(`http://localhost/kgwordpress/wp-json/wp/v2/posts?title=${tytul}&status=publish&content=${tresc}`,{
+        method:"POST",
+        headers:{
+            Authorization:`Basic ${btoa("Kamil:koronawirus2@2@")}`
+        }
+    })
+    const json = await data.json()
+    const point = document.createElement('div')
+    const li = document.createElement('li')
+    const butt = document.createElement('button')
+    li.innerHTML = json.title.rendered
+    butt.innerHTML = "DELETE"
+    butt.addEventListener('click',()=>{
+        deleteArticle(json.id)
+    })
+    point.setAttribute('id',json.id)
+    point.appendChild(li)
+    point.appendChild(butt)
+    document.getElementById('list').appendChild(point)
+}
